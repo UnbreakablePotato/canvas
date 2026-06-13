6ec8dbb7854f7283d38060634ffe8637d0877591
UnbreakablePotato <swipeytwit@gmail.com>
2026-06-13 15:19:09 +0200

    Added structure to receive and send json events

--- src/client.py
+++ src/client.py
@@ -2,17 +2,67 @@ import asyncio
 import websockets
 import aioconsole
 import json
+from create_canvas import x1, y1, x2, y2
 
-async def client():
+async def client(event=""):
     async with websockets.connect("ws://localhost:8000") as websocket:
-        write = asyncio.create_task(client_messenger(websocket))
+        write = asyncio.create_task(client_messenger(websocket, event))
         read = asyncio.create_task(client_listener(websocket))
 
         await read
         await write
 
 
-async def client_messenger(websocket):
+async def client_messenger(websocket,event):
+    #name = await aioconsole.ainput("Type your name:")
+    #message = await aioconsole.ainput()
+    event_json = {
+        "type": "draw",
+        "strokeId": "1234",
+        "color": "FF0000",
+        "linewidth": 5,
+        "x1": x1,
+        "y1": y1,
+        "x2": x2,
+        "y2": y2
+    }
+    #event = json.dumps(event_json)
+    while True:
+        event_json = {
+        "type": "draw",
+        "strokeId": "1234",
+        "color": "FF0000",
+        "linewidth": 5,
+        "x1": x1,
+        "y1": y1,
+        "x2": x2,
+        "y2": y2
+    }
+        await websocket.send(json.dumps(event))
+        #message = await aioconsole.ainput()
+
+
+
+
+async def client_listener(websocket):
+    while True:
+        response = await websocket.recv()
+        if response != "":
+            event = json.loads(response)
+        if event["type"] == "draw":
+            pass
+            #check who drew
+            #update local canvas with given data
+        elif event["type"] == "quit":
+            pass
+        elif event["type"] == "join":
+            pass
+        
+
+asyncio.run(client())
+
+
+"""
     name = await aioconsole.ainput("Type your name:")
     message = await aioconsole.ainput()
     event_json = {
@@ -23,7 +73,9 @@ async def client_messenger(websocket):
         "color": "FF0000",
         "linewidth": 5,
         "x": 4,
-        "y": 9
+        "y": 9,
+        "x2": 7,
+        "y2": 11
     }
     #event = json.dumps(event_json)
     while event_json["message"] != "quit":
@@ -34,15 +86,10 @@ async def client_messenger(websocket):
         "strokeId": "1234",
         "color": "FF0000",
         "linewidth": 5,
-        "x": 4,
-        "y": 9
+        "x1": 4,
+        "y1": 9,
+        "x2": 7,
+        "y2": 11
     }
-        await websocket.send(json.dumps(event_json))
-        message = await aioconsole.ainput()
 
-async def client_listener(websocket):
-    while True:
-        response = await websocket.recv()
-        print(response)
-
-asyncio.run(client())
+"""
